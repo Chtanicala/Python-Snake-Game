aa575912cf6fcc3a66741b4d28b24802f7cdf41e
Christian Tanicala <christian.tanicala@gmail.com>
2023-03-21 23:30:53 -0400

    const, functions, import modules, windows

---
+++ Snake.py
@@ -0,0 +1,33 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "green"
+FOOD_COLOR = "red"
+BACKGROUND_COLOR = "black"
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def game_over():
+    pass
+
+def check_collision():
+    pass
+
+window = Tk()
+
+window.mainloop()

1fac667ea80beeae6d7a9ef29f8b9721312c106a
Christian Tanicala <christian.tanicala@gmail.com>
2023-03-22 00:10:08 -0400

    snake and food eating/expanding

--- Snake.py
+++ Snake.py
@@ -11,52 +11,136 @@ FOOD_COLOR = "red"
 BACKGROUND_COLOR = "black"
 
 class Snake:
+
     def __init__(self):
 
         self.body_size = BODY_PARTS
+
         self.coordinates = []
+
         self.squares = []
 
+
+
         for i in range(0, BODY_PARTS):
+
             self.coordinates.append([0, 0])
 
+
+
         for x, y in self.coordinates:
-            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="Snake")
+
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+
             self.squares.append(square)
 
+
+
+
+
 class Food:
+
+
+
     def __init__(self):
-        
-        x = random.randint(0,(GAME_WIDTH/SPACE_SIZE-1)*SPACE_SIZE)
-        y = random.randint(0,(GAME_HEIGHT/SPACE_SIZE-1)*SPACE_SIZE)
 
-        self.coordinates = [x,y]
 
-        canvas.create_oval(x,y,x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+
+
+
+        self.coordinates = [x, y]
+
+
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+
+
+
+
 
 def next_turn(snake, food):
+
+
+
     x, y = snake.coordinates[0]
 
+
+
     if direction == "up":
+
         y -= SPACE_SIZE
+
     elif direction == "down":
+
         y += SPACE_SIZE
+
     elif direction == "left":
+
         x -= SPACE_SIZE
+
     elif direction == "right":
+
         x += SPACE_SIZE
 
-    snake.coordinates.insert(0,(x, y))
+
+
+    snake.coordinates.insert(0, (x, y))
+
+
 
     square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
 
-    snake.squares.insert(0,square)
 
-    del snake.coordinates[-1]
-    canvas.delete(snake.squares[-1])
-    del snake.squares[-1]
 
-    window.after(SPEED,next_turn, snake, food)
+    snake.squares.insert(0, square)
+
+
+
+    if x == food.coordinates[0] and y == food.coordinates[1]:
+
+
+
+        global score
+
+
+
+        score += 1
+
+
+
+        label.config(text="Score:{}".format(score))
+
+
+
+        canvas.delete("food")
+
+
+
+        food = Food()
+
+
+
+    else:
+
+
+
+        del snake.coordinates[-1]
+
+
+
+        canvas.delete(snake.squares[-1])
+
+
+
+        del snake.squares[-1]
+
+
+    window.after(SPEED, next_turn, snake, food)
+
 
 
 def change_direction(new_direction):
@@ -113,6 +197,6 @@ window.bind('<Down>', lambda event: change_direction('down'))
 snake = Snake()
 food = Food()
 
-next_turn(snake,food)
+next_turn(snake, food)
 
 window.mainloop()
